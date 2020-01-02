e10d43589e693d55016b7251d5e76d42868e2b09
ChangNoi <44426331+RioRocker97@users.noreply.github.com>
2020-01-02 13:02:32 +0700

    Initialize dynamic playground

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
+//initalize app with lots of thing i do not know
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+//declare router hear
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var dynamicRouter = require('./routes/dyna');
+//make web app with Express.js
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
+// something about using other feature in express.js
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/dynamic',dynamicRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
