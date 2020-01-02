239863abeb855dc4139cacb4748a5cf5bd99e6f7
ChangNoi <44426331+RioRocker97@users.noreply.github.com>
2020-01-02 19:25:29 +0700

    initialize mysql package into Express.js app

--- routes/dyna.js
+++ routes/dyna.js
@@ -4,4 +4,7 @@ var router = express.Router();
 router.get('/',function(req,res,next){
     res.render('dynamic1');
 });
+router.get('/d2',function(req,res,next){
+    res.render('dynamic2');
+})
 module.exports = router;
