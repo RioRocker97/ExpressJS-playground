82a8314f3a8696d34b9668fa962a42f2dcfda2c8
ChangNoi <44426331+RioRocker97@users.noreply.github.com>
2020-01-03 22:15:46 +0700

    Remove messy Controller
    it was a mistake . i thought i could make new Controller but it already there . Route IS controller DUH !

--- routes/dyna.js
+++ routes/dyna.js
@@ -4,7 +4,4 @@ var router = express.Router();
 router.get('/',function(req,res,next){
     res.render('dynamic1');
 });
-router.get('/d2',function(req,res,next){
-    res.render('dynamic2',{name:['abc','def','hij']});
-})
 module.exports = router;
