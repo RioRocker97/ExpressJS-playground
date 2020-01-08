346dbf59dd80d66d83433d76d03e7087fcdc6d09
ChangNoi <44426331+RioRocker97@users.noreply.github.com>
2020-01-08 21:37:16 +0700

    Second Database integration

--- model/car.js
+++ model/car.js
@@ -1,4 +1,14 @@
 var express = require('express');
 var myData = {d1: 'Mustang GT-40',d2: 'Ford',d3: '1965'};
+var mysql = require('mysql');
+var myDB = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'chang1997',
+    database: 'carlist'
+})
 
-module.exports = myData;
+module.exports ={
+    myData: myData,
+    myDB: myDB
+}
--- routes/dyna.js
+++ routes/dyna.js
@@ -3,8 +3,22 @@ var router = express.Router();
 
 //include my First Model
 var car = require('../model/car')
-
+var myCar = car.myData;
+var myDB = car.myDB;
 router.get('/',function(req,res,next){
-    res.render('dynamic1',{d1:car.d1,d2:car.d2,d3:car.d3});
+    myDB.query('SELECT * FROM car ',function(err,rows,fields){
+        if(err) throw err
+        var carDB= [];
+        for(var i=0;i<5;i++){
+            carDB[i]=rows[i].name;
+        }
+        console.log(carDB);
+        res.render('dynamic1',{
+            d1:myCar.d1,
+            d2:myCar.d2,
+            d3:myCar.d3,
+            db1:carDB
+        });
+    })
 });
 module.exports = router;
